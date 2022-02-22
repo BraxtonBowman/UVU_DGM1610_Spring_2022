1e6ae8221de189eade265ddd3e230eb617b733e5
Braxton Bowman <10923641@my.uvu.edu>
2022-02-22 01:36:41 -0700

    Added "Operators.cs" script

--- SpringClass/Assets/Operators.cs
+++ SpringClass/Assets/Operators.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class COperators : MonoBehaviour
+public class Operators : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        int x = 100 + 50;
+
+        int sum1 = 100 + 50;
+        int sum2 = sum1 + 250;
+        int sum3 = sum2 + sum2;
+
+        int x = 10;
+        x += 5;
     }
 
     // Update is called once per frame
